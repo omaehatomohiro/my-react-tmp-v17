131ea67578a509b72da81b16a6af5e4d5856add7
imac27 <tomohirohky@gmail.com>
2022-05-07 00:42:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# my-react-tmp-v17
+
+create-react-appのプロジェクト内容を、react v17 用にした。
+npmの依存関係も解決済みで、cloneしてすぐにプロジェクト作成を始められるように用意しただけ
+
+- react: ^17.0.0
+- react-dom: ^17.0.0
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from 'react';
+import { DragDropContext,Droppable,Draggable  } from 'react-beautiful-dnd';
+import './App.css';
+import { Home } from "./components/pages/Home";
+
+function App() {
+  return (
+    
+  )
+}
+
+export default App;
